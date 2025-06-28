95e38625374fbb08145d29648f76d2f3986545b0
Sangita  paul <sangitapaul101998@gmail.com>
2025-06-28 21:24:40 +0530

    Added fast api call and streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import datetime
+import requests
+import sys
+
+BASE_URL = "http://localhost:8000"
+
+st.set_page_config(
+    page_title="🌍 Travel Planner Agetic Application",
+    page_icon="🌍",
+    layout="centered",
+    initial_sidebar_state="expanded"
+)
+
+st.title("🌍 Travel Planner Agetic Application")
+
+#Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+st.header("How can I help you in planning a trip? Let me know where you wnat to visit.")
+
+with st.form(key="query_form", clear_on_submit=True):
+    user_input=st.text_input("User Input", placeholder="e.g. Plan a trip for 5 days")
+    submit_button=st.form_submit_button("Send")
+
+if submit_button and user_input.strip():
+    try:
+        with st.spinner("Bot is thinking...."):
+            payload = {"question": user_input}
+            response = requests.post(f"{BASE_URL}/query", json=payload)
+
+        if response.status_code == 200:
+            answer = response.json().get("answer", "No answer returned")
+            markdown_content = f"""# 🌍 AI Travel Plan
+
+            -------
+            {answer}
+            -------
+
+            *This travel plan is created by AI. Please verify all information, especially prices, operating hours, and travel requirements before your trip.*
+            """
+
+            st.markdown(markdown_content)
+        else:
+            st.error(" Bot failed to respond: " + response.text)
+
+    except Exception as e:
+        raise f"The reponse failed due to {e}"
